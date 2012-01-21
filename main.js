465cbed4b154b17afabbe596a97bb74e836d2707
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-01-21 17:31:15 -0200

    add 'Invert' feature

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 function setOptionsPanel() {
     var div = $('div.pagehead.shrunken.dashboard');
     div.append('\
-        <div id="dash_options">\
-            <input type="checkbox" checked id="watch_started"> Watch </input>\
-            <input type="checkbox" checked id="follow"> Follow </input>\
-            <input type="checkbox" checked id="push"> Push </input>\
-            <input type="checkbox" checked id="create"> Create </input>\
-            <input type="checkbox" checked id="fork"> Fork </input>\
-            <input type="checkbox" checked id="issues_opened"> Opened Issue</input>\
-            <input type="checkbox" checked id="issues_closed"> Closed Issue</input>\
-            <input type="checkbox" checked id="issues_comment"> Commented Issue</input>\
-            <input type="checkbox" checked id="gist"> Gist </input>\
-            <input type="checkbox" checked id="gollum"> Gollum </input>\
+        <div id="dash_filter">\
+            <div id="dash_options">\
+                <input type="checkbox" checked id="watch_started"> Watch </input>\
+                <input type="checkbox" checked id="follow"> Follow </input>\
+                <input type="checkbox" checked id="push"> Push </input>\
+                <input type="checkbox" checked id="create"> Create </input>\
+                <input type="checkbox" checked id="fork"> Fork </input>\
+                <input type="checkbox" checked id="issues_opened"> Open Issue</input>\
+                <input type="checkbox" checked id="issues_closed"> Close Issue</input>\
+                <input type="checkbox" checked id="issues_comment"> Comment Issue</input>\
+                <input type="checkbox" checked id="gist"> Gist </input>\
+                <input type="checkbox" checked id="gollum"> Gollum </input>\
+            </div>\
+            <div id="filter_helper">\
+                <a href="javascript:void(0);" id="invert"> Invert </a>\
+            </div>\
         </div>\
     ');
+
+    $('#invert').click(invertOptions);
 
     var inputs = div.find('input');
     inputs.change(function() {
@@ -59,6 +66,17 @@ function filterDashboard() {
     })
 }
 
+//############################### Filter Options helper #########################################
+
+function invertOptions() {
+    $('#dash_options').children().each(function() {
+        $(this).attr('checked') ? $(this).removeAttr('checked') : $(this).attr('checked', 'checked');
+    });
+    setOptionsIntoLocalStorage();
+    filterDashboard();
+}
+
+//###############################################################################################
 
 (function main() {
     setOptionsPanel();
